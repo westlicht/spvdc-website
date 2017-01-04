562fa9f0c28fa5eb5fa6d2f1a3ac72dc902dbfff
Simon Kallweit <simon@weirdsoft.ch>
2017-01-04 14:11:07 +0100

    use data from contact YML in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,9 @@ import { Icon } from "react-fa"
 import PhoneNumber from "../PhoneNumber"
 import EmailAddress from "../EmailAddress"
 
-const Footer = (props, context) => {
+const contact = require("../../../content/contact/data.yml")
 
-  const data = {
-    de: require("../../../content/de/footer.yml"),
-    fr: require("../../../content/fr/footer.yml"),
-    en: require("../../../content/en/footer.yml"),
-  }[context.locale]
+const Footer = (props, context) => {
 
   return (
     <div id="footer">
@@ -20,7 +16,7 @@ const Footer = (props, context) => {
             <div className="col">
               <h1>Adresse</h1>
               <p>
-                {data.address.split("\n").map(function(line) {
+                {contact.address[context.locale].split("\n").map(function(line) {
                   return (
                     <span>
                       {line}
@@ -35,19 +31,19 @@ const Footer = (props, context) => {
               <p>
                 <span>
                   <Icon name="phone" fixedWidth={ true } />
-                  <PhoneNumber address={ data.contact.tel } />
+                  <PhoneNumber address={ contact.tel } />
                 </span><br/>
                 <span>
                   <Icon name="fax" fixedWidth={ true } />
-                  <PhoneNumber address={ data.contact.fax } link={ false } />
+                  <PhoneNumber address={ contact.fax } link={ false } />
                 </span><br/>
                 <span>
                   <Icon name="envelope-o" fixedWidth={ true } />
-                  <EmailAddress address={ data.contact.email } />
+                  <EmailAddress address={ contact.email } />
                 </span><br/>
                 <span>
                   <Icon name="globe" fixedWidth={ true } />
-                  <a href={ "http://" + data.contact.www }>{ data.contact.www }</a>
+                  <a href={ "http://" + contact.www }>{ contact.www }</a>
                 </span><br/>
               </p>
             </div>
