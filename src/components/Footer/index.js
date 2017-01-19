51728c2cd767adeb088509801d9783ceb1dde944
Simon Kallweit <simon@weirdsoft.ch>
2017-01-19 12:05:53 +0100

    work on footer styles

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -22,58 +22,63 @@ const Footer = (props, context) => {
   }))
 
   return (
-    <div id="footer">
+    <footer id="footer">
       <div className="container">
-        <section>
-          {/* <div className="row"> */}
-            <div className="col">
-              <h1>
-                <FormattedMessage id="footer.address" defaultMessage="Address" />
-              </h1>
-              <p>
-                {
-                  ContactData.address(context.locale).map((line, id) => (
-                    <span key={ id } className="line">
-                      {line}
-                    </span>
-                  ))
-                }
-              </p>
-            </div>
-            <div className="col">
-              <h1>
-                <FormattedMessage id="footer.contact" defaultMessage="Contact" />
-              </h1>
-              <p>
-                <span className="line">
-                  <Icon name="phone" fixedWidth={ true } />
-                  <PhoneNumber address={ ContactData.data.contact.phone } />
-                </span>
-                <span className="line">
-                  <Icon name="envelope-o" fixedWidth={ true } />
-                  <EmailAddress address={ ContactData.data.contact.email } />
-                </span>
-                <span className="line">
-                  <Icon name="globe" fixedWidth={ true } />
-                  <a href={ "http://" + ContactData.data.contact.www }>{ ContactData.data.contact.www }</a>
-                </span>
-              </p>
-            </div>
-            <div className="col">
-              <h1>
-                <FormattedMessage id="footer.times" defaultMessage="Times" />
-              </h1>
-              <p>
-                {
-                  ContactData.times(context.locale).map((line, id) => (
-                    <span key={ id } className="line">
-                      {line}
-                    </span>
-                  ))
-                }
-              </p>
-            </div>
-        </section>
+        <div className="infos">
+          <div className="address">
+            <h1>
+              <FormattedMessage id="footer.address" defaultMessage="Address" />
+            </h1>
+            <p>
+              {
+                ContactData.address(context.locale).map((line, id) => (
+                  <span key={ id } className="line">
+                    {line}
+                  </span>
+                ))
+              }
+            </p>
+          </div>
+          <div className="contact">
+            <h1>
+              <FormattedMessage id="footer.contact" defaultMessage="Contact" />
+            </h1>
+            <p>
+              <span className="line">
+                <Icon name="phone" fixedWidth={ true } />
+                <PhoneNumber address={ ContactData.data.contact.phone } />
+              </span>
+              <span className="line">
+                <Icon name="envelope-o" fixedWidth={ true } />
+                <EmailAddress address={ ContactData.data.contact.email } />
+              </span>
+              <span className="line">
+                <Icon name="globe" fixedWidth={ true } />
+                <a href={ "http://" + ContactData.data.contact.www }>{ ContactData.data.contact.www }</a>
+              </span>
+            </p>
+          </div>
+          <div className="hours">
+            <h1>
+              <FormattedMessage id="footer.hours" defaultMessage="Hours" />
+            </h1>
+            <table>
+              {
+                ContactData.times(context.locale).map((line, id) => (
+                  <tr key={ id }>
+                    {
+                      line.map((item, id) => (
+                        <td key={ id }>
+                          { item }
+                        </td>
+                      ))
+                    }
+                  </tr>
+                ))
+              }
+            </table>
+          </div>
+        </div>
       </div>
       <GoogleMap
         className="map"
@@ -82,10 +87,10 @@ const Footer = (props, context) => {
         lng={ ContactData.data.map.lng }
         zoom={ ContactData.data.map.zoom }
       />
-      <div className="container2">
+      <div className="container">
         <NavigationMenu id="nav-footer" items={ navigationItems } />
       </div>
-    </div>
+    </footer>
   )
 }
 
