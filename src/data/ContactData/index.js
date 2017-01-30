68436408c38bb2251df5b07cee802c29fc02e607
Simon Kallweit <simon@weirdsoft.ch>
2017-01-30 12:19:40 +0100

    added language to members

--- src/data/ContactData/index.js
+++ src/data/ContactData/index.js
@@ -34,6 +34,7 @@ class ContactData {
       image: item.image || "/assets/img/team/placeholder.jpg",
       name: item.firstName + " " + item.lastName,
       title: translatedString(item.title, locale),
+      language: translatedString(item.language, locale),
       email: item.email,
       phone: item.phone,
       vcard: "/assets/" + locale + "/contact/" + item.firstName.toLowerCase() + "-" + item.lastName.toLowerCase() + ".vcf",
