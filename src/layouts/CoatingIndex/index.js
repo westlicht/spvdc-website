0e935ec6a1d5ddbe703ca6fa5ef7f2df3142a285
Simon Kallweit <simon@weirdsoft.ch>
2017-02-09 15:23:44 +0100

    cleanup

--- src/layouts/CoatingIndex/index.js
+++ src/layouts/CoatingIndex/index.js
@@ -1,14 +1,11 @@
 import React, { PropTypes } from "react"
 import { BodyContainer } from "phenomic"
-// import enhanceCollection from "phenomic/lib/enhance-collection"
 import isBrowser from "../../utils/isBrowser"
 
 import PageWrapper from "../PageWrapper"
 
 import Section from "../../components/Section"
 import CoatingFinderContainer from "../../containers/CoatingFinderContainer"
-
-// import CoatingData from "../../data/CoatingData"
 
 // import styles from "./index.css"
 
@@ -30,10 +27,5 @@ CoatingIndex.propTypes = {
   head: PropTypes.object.isRequired,
   body: PropTypes.string,
 }
-
-// CoatingIndex.contextTypes = {
-//   collection: PropTypes.array.isRequired,
-//   locale: PropTypes.string.isRequired,
-// }
 
 export default CoatingIndex
