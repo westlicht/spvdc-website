0c108e98678466a4e5cf70f6ac51eda2a5d6a6ea
Simon Kallweit <simon@weirdsoft.ch>
2017-02-10 11:02:52 +0100

    use div for banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -2,12 +2,12 @@ import React, { PropTypes } from "react"
 
 const Banner = ({ image, children }) => {
   return (
-    <section
+    <div
       id="banner"
       style={{ backgroundImage: "url(" + image + ")" }}
     >
       { children }
-    </section>
+    </div>
   )
 }
 
