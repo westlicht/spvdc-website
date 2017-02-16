cd573f3de71030208f755cc3f09e979cc186e136
Simon Kallweit <simon@weirdsoft.ch>
2017-02-16 10:31:40 +0100

    refactored footnote generation on coating page

--- src/layouts/Coating/index.js
+++ src/layouts/Coating/index.js
@@ -1,11 +1,12 @@
 import React, { PropTypes } from "react"
 import { FormattedMessage } from "react-intl"
 
+import _ from "lodash"
+
 const Carousel = require('react-responsive-carousel').Carousel
 require("react-responsive-carousel/lib/styles/carousel.min.css")
 
 import isBrowser from "../../utils/isBrowser"
-
 
 import transformMarkdown from "../../transform-markdown"
 import RawHtml from "react-raw-html"
@@ -31,26 +32,47 @@ const Coating = (props, { locale }) => {
   const fields = CoatingData.fields
   const coating = CoatingData.coatings[id]
 
-  // create specification table
   let footnotes = []
-  const specifications = fields.map(field => {
-    let title = field.title[locale]
-    if (field.footnote) {
-      footnotes.push(field.footnote[locale])
-      title += " " + "*".repeat(footnotes.length)
-    }
-    title = (<RawHtml.span>{ transformMarkdown(title) }</RawHtml.span>)
+  let footnoteIndex = 0
+  const addFootnote = (body) => {
+    footnoteIndex += 1
+    footnotes.push(
+      <div
+        id={ "footnote" + footnoteIndex }
+        className={ styles.footnote + " anchor" }
+        key={ footnoteIndex }
+      >
+        <span className={ styles.footnoteAnchor }>{ "[" + footnoteIndex + "]" }</span>
+        <RawHtml.p className={ styles.footnoteContent }>{ transformMarkdown(body) }</RawHtml.p>
+      </div>
+    )
+    return (<span key="footnote"><sup><a href={ "#footnote" + footnoteIndex }>{ footnoteIndex }</a></sup></span>)
+  }
 
-    const data = coating.fields[field.id]
-    let value = data ? translatedString(data) : "-"
-    if (field.units) {
-      value += " " + field.units
-    }
-    return {
-      title: title,
-      value: value,
-    }
+  const titles = fields.map(field => {
+    const titleText = translatedString(field.title, locale)
+    return [
+      (<RawHtml.span key="value">{ titleText }</RawHtml.span>),
+      field.footnote ? addFootnote(translatedString(field.footnote, locale)) : null,
+    ]
   })
+
+  const values = fields.map(field => {
+    const data = coating.fields[field.id]
+    const valueText = [
+      (data && data.value) ? String(translatedString(data.value, locale)) : "-",
+      field.units,
+    ].join(" ")
+    return [
+      (<RawHtml.span key="value">{ valueText }</RawHtml.span>),
+      (data && data.footnote) ? addFootnote(translatedString(data.footnote, locale)) : null,
+    ]
+  })
+
+  const specifications = _.zip(titles, values).map(item => ({
+    title: item[0],
+    value: item[1],
+  }))
 
   return (
     <PageWrapper { ...props } bannerImage="/assets/img/banner/coatings.jpg">
@@ -66,8 +88,8 @@ const Coating = (props, { locale }) => {
             <div className={ styles.image }>
               <Carousel showArrows={ false }  showStatus={ false } showThumbs={ false }>
                 {
-                  coating.images.map(image => (
-                    <img src={ image } />
+                  coating.images.map((image, index) => (
+                    <img src={ image } key={ index }/>
                   ))
                 }
               </Carousel>
@@ -77,16 +99,14 @@ const Coating = (props, { locale }) => {
 
         <div className={ styles.container }>
           <div className={ styles.left }>
-            <div className={ styles.specs }>
+            <div className={ styles.specification }>
               <h3>
                 <FormattedMessage id="coatings.specification" defaultMessage="Specification" />
               </h3>
               <SimpleTable rows={ specifications } />
-              {
-                footnotes.map((footnote, index) => (
-                  <p key={ index }>{ "*".repeat(index + 1) + " " + footnote }</p>
-                ))
-              }
+              <div className={ styles.footnotes }>
+                { footnotes }
+              </div>
             </div>
           </div>
           <div className={ styles.right }>
