4d191c7c25f5ed507914ac63d5f3a5c600bba0fc
Simon Kallweit <simon@weirdsoft.ch>
2017-05-03 09:01:51 +0200

    updated upload folder

--- scripts/deploy-belele.sh
+++ scripts/deploy-belele.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 SRC=dist/
-DST=freak@belele.inthemill.ch:/var/www/swiss-pvd-coating.ch/beta
+DST=freak@belele.inthemill.ch:/var/www/swiss-pvd-coating.ch/www
 
 rsync -avzr --delete ${SRC} ${DST}
